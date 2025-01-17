fb1174fd1a00eacf686803091002f46527b240b1
opilane <opilane@HP-CZC0359NN5>
2025-01-17 10:44:23 +0200

    Add project files.

---
+++ MatemaatilineKoer1/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MatemaatilineKoer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage esimene arv:");
+            string firstNumber = Console.ReadLine();
+            float floatFirst = float.Parse(firstNumber);
+
+            Console.WriteLine("Sisestage teine arv:");
+            string secondNumber = Console.ReadLine();
+            float floatSecond = float.Parse(secondNumber);
+
+            string dog = Console.ReadLine();
+
+            if (dog == "+")
+            {
+                Console.WriteLine(floatFirst + floatSecond);
+            }
+            else if (dog == "-")
+            {
+                Console.WriteLine(floatFirst - floatSecond);
+            }
+            else if (dog == "*")
+            {
+                Console.WriteLine(floatFirst * floatSecond);
+            }
+            else if (dog == "/")
+            {
+                Console.WriteLine(floatFirst / floatSecond);
+            }
+
+
+        }
+    }
+}
